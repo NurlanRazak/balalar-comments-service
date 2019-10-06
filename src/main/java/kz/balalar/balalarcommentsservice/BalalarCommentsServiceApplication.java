b7b730f0b97945f3ff9b24a0b2c31bc6091ed9ee
Nurlan <nurik.droid@gmail.com>
2019-10-06 23:30:55 +0600

    service discovery

--- src/main/java/kz/balalar/balalarcommentsservice/BalalarCommentsServiceApplication.java
+++ src/main/java/kz/balalar/balalarcommentsservice/BalalarCommentsServiceApplication.java
@@ -2,8 +2,10 @@ package kz.balalar.balalarcommentsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BalalarCommentsServiceApplication {
 
 	public static void main(String[] args) {
